1785c4d5c915999990fb0f9a26888129b9c75991
Jared Serrao <jared.serrao5@gmail.com>
2023-05-12 12:27:17 -1000

    test

--- chamber/js/date.js
+++
@@ -1,6 +0,0 @@
-const date = new Date();
-const year = date.getFullYear();
-
-
-document.querySelector("#year").textContent = year;
-document.getElementById("updated").textContent = `Last Updated: ${document.lastModified}`;
