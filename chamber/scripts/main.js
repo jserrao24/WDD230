c8252e0d03b0309bc2a214c34feacff773f3a0e6
Jared Serrao <jared.serrao5@gmail.com>
2023-05-12 12:28:22 -1000

    test

---
+++ chamber/scripts/main.js
@@ -0,0 +1,20 @@
+const lastUpdated = new Date(document.lastModified);
+document.querySelector('#lastUpdated').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
+document.querySelector('#lastUpdated-1').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
+const currentDate = new Date();
+document.querySelector('#currentyear').textContent = currentDate.getFullYear();
+document.querySelector('#currentyear-1').textContent = currentDate.getFullYear();
+const date = document.querySelector("#currentdate");
+try {
+    const option = {
+        weekday: "long"
+    }
+	const options = {
+		month: "long",
+		day: "numeric",
+		year: "numeric"
+	};
+	date.innerHTML = `${new Date().toLocaleDateString("en-UK", option)}, ${new Date().toLocaleDateString("en-UK", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+};
