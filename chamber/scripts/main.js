66ec4dc564b07918bcb57dc67acbf0e8f04bc453
Jared Serrao <jared.serrao5@gmail.com>
2023-05-18 21:51:42 -1000

    tst

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,20 +1,27 @@
+const banner = document.getElementById("banner");
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+
+if (currentDay === 1 || currentDay === 2) {
+  banner.style.display = "block";
+}
+
 const lastUpdated = new Date(document.lastModified);
 document.querySelector('#lastUpdated').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
 document.querySelector('#lastUpdated-1').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
-const currentDate = new Date();
 document.querySelector('#currentyear').textContent = currentDate.getFullYear();
 document.querySelector('#currentyear-1').textContent = currentDate.getFullYear();
 const date = document.querySelector("#currentdate");
 try {
     const option = {
         weekday: "long"
-    }
-	const options = {
-		month: "long",
-		day: "numeric",
-		year: "numeric"
-	};
-	date.innerHTML = `${new Date().toLocaleDateString("en-UK", option)}, ${new Date().toLocaleDateString("en-UK", options)}</span>`;
+    };
+    const options = {
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+    };
+    date.innerHTML = `${currentDate.toLocaleDateString("en-UK", option)}, ${currentDate.toLocaleDateString("en-UK", options)}</span>`;
 } catch (e) {
-	console.log("Error with code or your browser does not support Locale");
-};
+    console.log("Error with code or your browser does not support Locale");
+}
