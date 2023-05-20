0cb77232636e8c52606764397fb6e7d80dca2365
Jared Serrao <jared.serrao5@gmail.com>
2023-05-19 22:55:45 -1000

    test

--- chamber test/scripts/main.js
+++ chamber test/scripts/main.js
@@ -1,43 +1,28 @@
-// Header and footer date outputs
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("open");
-  document.getElementById("hamburgerBtn").classList.toggle("open");
-}
+const lastUpdated = new Date(document.lastModified);
+document.querySelector('#lastUpdated').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
+document.querySelector('#lastUpdated-1').textContent = `${lastUpdated.toLocaleDateString()} at ${lastUpdated.toLocaleTimeString()}`;
+const currentDate = new Date();
+document.querySelector('#currentyear').textContent = currentDate.getFullYear();
+document.querySelector('#currentyear-1').textContent = currentDate.getFullYear();
+const date = document.querySelector("#currentdate");
+try {
+    const option = {
+        weekday: "long"
+    }
+	const options = {
+		month: "long",
+		day: "numeric",
+		year: "numeric"
+	};
+	date.innerHTML = `${new Date().toLocaleDateString("en-UK", option)}, ${new Date().toLocaleDateString("en-UK", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+};
 
-const x = document.getElementById("hamburgerBtn");
-
-x.onclick = toggleMenu;
-
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const month = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-const d = new Date();
-
-document.getElementById("today").textContent = `${weekday[d.getDay()]}, ${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
-document.getElementById("modified").textContent = `Last Updated: ${document.lastModified}`;
-
-// Join us message based on day of the week
-if (d.getDay() == 1 || d.getDay() == 2) {
-  document.getElementById("joinUs").style.display = "block";
-}
+const divi = document.querySelector('div');
+if (currentDate.getDay() == 1 || currentDate.getDay() == 2) {
+    const para = document.createElement('p');
+    divi.appendChild(para);
+    para.textContent = "🤝🏼 Come join us for the chamber meet & greet Wednesday at 7:00 p.m. 🤝🏼";
+	divi.style.display = "block";
+} else {divi.style.display = "none";}
