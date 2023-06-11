f0ba11dbc9b74f3f145f926377064b13bf7594e4
Jared Serrao <jared.serrao5@gmail.com>
2023-06-11 10:11:45 -1000

    test

--- presentation_example_1/example1.js
+++ presentation_example_1/example1.js
@@ -1,16 +1,14 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    var finished_rendering = function() {
-        console.log("finished rendering plugins");
-        var spinner = document.getElementById("spinner");
-        spinner.removeAttribute("style");
-        spinner.removeChild(spinner.childNodes[0]);
-    };
-
-    FB.Event.subscribe('xfbml.render', finished_rendering);
-
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            console.log(response.authResponse.accessToken);
-        }
-    });
-});
+var finished_rendering = function() {
+    console.log("finished rendering plugins");
+    var spinner = document.getElementById("spinner");
+    spinner.removeAttribute("style");
+    spinner.removeChild(spinner.childNodes[0]);
+  }
+  FB.Event.subscribe('xfbml.render', finished_rendering);
+  
+  
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      console.log(response.authResponse.accessToken);
+    }
+  });
