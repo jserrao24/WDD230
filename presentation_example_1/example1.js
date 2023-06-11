9199a6db460a1208e865856543c1008306fbbf62
Jared Serrao <jared.serrao5@gmail.com>
2023-06-11 10:56:27 -1000

    test

--- presentation_example_1/example1.js
+++ presentation_example_1/example1.js
@@ -1,18 +1,34 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    var finished_rendering = function() {
-        console.log("finished rendering plugins");
-        var spinner = document.getElementById("spinner");
-        spinner.removeAttribute("style");
-        spinner.removeChild(spinner.childNodes[0]);
-    };
-
-    FB.Event.subscribe('xfbml.render', finished_rendering);
-
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            console.log('Logged in successfully.');
-            console.log('User ID: ' + response.authResponse.userID);
-            console.log('Access Token: ' + response.authResponse.accessToken);
-        }
+// Initialize the Facebook SDK
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '233884559426921',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v11.0'
     });
-});
+  };
+  
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  
+  // Function called after the user logs in
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        // The user is logged in and authorized your app
+        var accessToken = response.authResponse.accessToken;
+        console.log('Access Token:', accessToken);
+        // You can now make API calls using the access token
+        // For example: FB.api('/me', {fields: 'name,email'}, function(response) { console.log(response); });
+      } else {
+        // The user is logged out or has not authorized your app
+        console.log('User is not logged in.');
+      }
+    });
+  }
