211b4083ea5fecfc86e3588c4f6a88f2dc340565
Jared Serrao <jared.serrao5@gmail.com>
2023-06-13 14:24:57 -1000

    test

--- lesson09/prophets.js
+++ lesson09/prophets.js
@@ -13,25 +13,36 @@ const displayProphets = (prophets) => {
 
   prophets.forEach((prophet) => {
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
+    card.classList.add('card');
+
+    // Create the HTML elements for name, birthdate, birthplace, death date, and number of children
+    let name = document.createElement('h2');
+    name.textContent = `${prophet.name} ${prophet.lastname}`;
+
     let birthdate = document.createElement('p');
+    birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
+
     let birthplace = document.createElement('p');
+    birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
 
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '340');
-    portrait.setAttribute('height', '440');
+    let death = document.createElement('p');
+    death.textContent = `Death: ${prophet.death || 'N/A'}`; // Show "N/A" if death date is null
 
-    birthdate.textContent = `Birth Date: ${prophet.birthdate}`;
-    birthplace.textContent = `Birth Place: ${prophet.birthplace}`;
+    let numofchildren = document.createElement('p');
+    numofchildren.textContent = `Number of Children: ${prophet.numofchildren}`;
 
-    card.appendChild(h2);
-    card.appendChild(portrait);
+    // Create an image element
+    let image = document.createElement('img');
+    image.src = prophet.imageurl;
+    image.alt = `${prophet.name} ${prophet.lastname}`;
+
+    // Append all the elements to the card
+    card.appendChild(name);
     card.appendChild(birthdate);
     card.appendChild(birthplace);
+    card.appendChild(death);
+    card.appendChild(numofchildren);
+    card.appendChild(image); // Append the image element
 
     cards.appendChild(card);
   });
