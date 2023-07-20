398659f382bd9d0ac3dca83d715abe4930b9aca7
Jared Serrao <jared.serrao5@gmail.com>
2023-07-19 17:59:10 -1000

    test

--- bountiful-foods/scripts/speciality.js
+++
@@ -1,9 +0,0 @@
-const numberDisplay = document.querySelector(".number1");
-const number = Number(window.localStorage.getItem("lastNumberDrink-ls")) || 0;
-if (number == '0') {
-    numberDisplay.textContent = `You have not submitted any specialty drinks yet.`;
-} else if (number == '1') {
-		numberDisplay.textContent = `You have submitted ` + number + ` specialty drink.`;
-} else {
-		numberDisplay.textContent = `You have submitted ` + number + ` speciality drinks.`;
-}
